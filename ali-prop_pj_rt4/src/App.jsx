ff260b0b87886c158c6856e065c49da7d151add8
Mohdali644 <Envied94@gmail.com>
2026-08-10 16:58:17 +0530

    Added new files

---
+++ ali-prop_pj_rt4/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Card />
+      <Card /> */}
+
+      <User />
+    </div>
+  )
+}
+
+export default App
