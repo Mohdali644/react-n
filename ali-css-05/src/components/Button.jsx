a1454b748c454c3fe328b2bd4063a98a24a9cca3
Mohdali644 <Envied94@gmail.com>
2026-08-15 23:30:30 +0530

    added properties and components

---
+++ ali-css-05/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+        <h3>Button Section</h3>
+      <button className='btn'>Click me!</button>
+    </div>
+  )
+}
+
+export default Button
