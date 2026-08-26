ae25d8f2e50acc7bed8acff3fa2d0398f3a6d0f5
Mohdali644 <Envied94@gmail.com>
2026-08-26 20:57:52 +0530

    feat: Add components and completed proj

--- ali-ui-pj-07/ali-ui-pj-07/src/components/Section1/RightCardContent.jsx
+++ ali-ui-pj-07/ali-ui-pj-07/src/components/Section1/RightCardContent.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { ArrowRight } from "lucide-react";
 
-const RightCardContent = () => {
+const RightCardContent = (props) => {
   return (
     <div>
       <div className="absolute top-0 left-0 p-8 flex flex-col justify-between h-full w-full">
-        <h2  className="w-10 h-10 text-2xl font-semi bg-white rounded-full flex justify-center items-center">1</h2>
+        <h2  className="w-10 h-10 text-2xl font-medium bg-white rounded-full flex justify-center items-center">{props.id+1}</h2>
         <div>
-          <p className="text-lg leading-relaxed text-white">
+          <p className="text-lg leading-relaxed text-shadow-2xs text-white">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
             corporis qui sapiente cum accusamus voluptatem.
           </p>
           <div className="pt-8 flex items-center gap-10">
           <button className="bg-blue-600 text-xl text-center text-white px-8 py-2 rounded-full">
-            Satisfied
+            {props.tag}
           </button>
           <button className="bg-blue-600 text-white px-3 py-2 rounded-full">
             <ArrowRight />
--- ali-ui-pj-07/ali-ui-pj-07/src/components/Section1/RightContent.jsx
+++ ali-ui-pj-07/ali-ui-pj-07/src/components/Section1/RightContent.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import RightCard from './RightCard'
 
-const RightContent = () => {
+const RightContent = (props) => {
+  console.log(props.users)
   return (
-    <div className='h-full flex flex-nowrap gap-8 p-6 w-2/3'>
-      <RightCard />
-      <RightCard />
-      <RightCard />
-      <RightCard />
-      <RightCard />
+    <div id='right' className='h-full flex flex-nowrap overflow-x-auto gap-8 p-6 w-2/3'>
+      {props.users.map(function(elem,idx){
+
+        return <RightCard key={idx} id={idx} img={elem.img} tag={elem.tag} />
+      })}
     </div>
   )
 }
