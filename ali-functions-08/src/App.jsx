ca39e85e6fed45023c548146367b616f78bebd22
Mohdali644 <Envied94@gmail.com>
2026-09-05 21:06:04 +0530

    feat: Add new files and track functionality

--- ali-functions-08/src/App.jsx
+++ ali-functions-08/src/App.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 
 const App = () => {
-function btnClicked() {
-  console.log("Button was Clicked")
+
+const scroll = (elem) => {
+  if(elem>0) {
+    console.log("Scrolling Correct(forward)",elem)
+  }else{
+    console.log("Scrolling Wrong(backward)",elem)
+  }
 }
 
+
+// function btnClicked() {
+//   console.log("Button was Clicked")
+// }
+
   return (
-    <div>
-      <button onClick={btnClicked}>Click me</button>
+    <div onWheel={(elem) => {
+      scroll(elem.deltaY)
+    }}>
+
+
+      {/* <button onClick={btnClicked}>Click me</button> */}
+
+    {/*---------------------------------------------------------------------- */}
 
       {/* <button onMouseEnter={function(){
         console.log("mouse captured")
       }}>New button</button> */}
 
+      {/*--------------------------------------------------------------------- */}
+
         {/* <input type='text' placeholder='Enter Name' onChange={function(elem) {
           console.log(elem.target.value)
         }}></input> */}
+
+      <div className='page1'></div>
+      <div className='page2'></div>
+      <div className='page3'></div>
 
     </div>
   )
